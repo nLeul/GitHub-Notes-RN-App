effe621482726b1c64b4005b45bf5452be3209d2
nLeul <leulnecha@gmail.com>
2020-05-05 23:53:28 -0500

    added notes component

--- components/Notes.js
+++ components/Notes.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
-    StyleSheet,
+    StyleSheet, KeyboardAvoidingView, View, ScrollView, TextInput, TouchableOpacity, Text
 } from 'react-native';
 
 import Badge from 'components/Badge';
@@ -8,9 +8,54 @@ import Separator from 'components/Separator';
 
 
 
-function Notes({ route }) {
 
-    return (null);
+function Notes({ route: { params: { user } } }) {
+
+    const [state, setState] = useState({ notes: [], note: '' });
+    const { notes, note } = state;
+
+    const renderNote = (note, index) => {
+        return (
+            <View key={index} style={styles.rowContainer}>
+                <Text>{note}</Text>
+                <Separator />
+            </View>
+        )
+    };
+
+    const handleNote = note => {
+        setState(prev => ({ ...prev, note }));
+
+    };
+
+    const handleSubmit = () => {
+        setState(prev => ({ notes: prev.notes.concat(prev.note), note: '' }));
+    };
+
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <Badge userInfo={user} />
+            <ScrollView>
+                {notes.map(note => renderNote(note))}
+            </ScrollView>
+            <View style={styles.footerContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    value={note}
+                    onChangeText={handleNote}
+                    placeHolder='New note'
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={handleSubmit}
+                >
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+
+            </View>
+            <View style={{height:100,backgroundColor:'#333'}}/>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
