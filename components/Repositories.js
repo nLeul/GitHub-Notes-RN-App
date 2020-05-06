f60e8ddecb7531047eb83ee42162e9d8afed8a1c
nLeul <leulnecha@gmail.com>
2020-05-05 22:46:48 -0500

    repo component done

--- components/Repositories.js
+++ components/Repositories.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    StyleSheet,
+    StyleSheet, ActivityIndicator, Text, View, ScrollView, TouchableOpacity
 } from 'react-native';
 
 import Separator from 'components/Separator';
 import Badge from 'components/Badge';
 
-function Repositories({ navigation, route }) {
 
-    return (null)
+function Repositories({ navigation: { navigate }, route: { params: { user } } }) {
+
+    const [state, setState] = useState({ list: [], loading: true, error: false });
+    const { list, loading, error } = state;
+
+    useEffect(() => {
+        fetch(user.repos_url)
+            .then(response => response.json())
+            .then(data => {
+
+                const list_elements = data.map((repo, index) => {
+                    return (
+                        <View key={index} style={styles.rowContainer}>
+                            <TouchableOpacity onPress={() => navigate('WEBVIEW', { url: repo.html_url })}>
+                                <Text style={styles.name}>{repo.name}</Text>
+                            </TouchableOpacity>
+                            <Text style={styles.description}>{repo.description ? repo.description : ''}</Text>
+                            <Separator />
+                        </View>
+                    );
+                });
+                setState(prevState => ({ ...prevState, loading: false, list: list_elements }))
+
+            })
+            .catch(error => setState(prevState => ({ ...prevState, loading: false, error: true })))
+    }, []);
+
+    return (
+        <ScrollView style={styles.container}>
+            <Badge userInfo={user} />
+            {loading ? <ActivityIndicator size='large' /> : list}
+            {error && <Text> Something went wrong!</Text>}
+        </ScrollView>
+
+    );
 }
 
 const styles = StyleSheet.create({
